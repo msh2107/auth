d96b8ec649f955bd925680c5f3e0cfff48d775ca
msh <shink@>
2023-10-18 13:48:00 +0300

    core: DI container added

---
+++ internal/app/service-provider.go
@@ -0,0 +1,105 @@
+package app
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/msh2107/auth/internal/api/user"
+	"github.com/msh2107/auth/internal/closer"
+	"github.com/msh2107/auth/internal/config"
+	"github.com/msh2107/auth/internal/config/env"
+	"github.com/msh2107/auth/internal/repository"
+	userRepository "github.com/msh2107/auth/internal/repository/user"
+	"github.com/msh2107/auth/internal/service"
+	userService "github.com/msh2107/auth/internal/service/user"
+)
+
+type serviceProvider struct {
+	pgConfig   config.PGConfig
+	grpcConfig config.GRPCConfig
+
+	pgPool         *pgxpool.Pool
+	userRepository repository.UserRepository
+
+	userService service.UserService
+
+	userImpl *user.Implementation
+}
+
+func newServiceProvider() *serviceProvider {
+	return &serviceProvider{}
+}
+
+func (s *serviceProvider) PGConfig() config.PGConfig {
+	if s.pgConfig == nil {
+		cfg, err := env.NewPGConfig()
+		if err != nil {
+			log.Fatalf("failed to get pg config: %s", err.Error())
+		}
+
+		s.pgConfig = cfg
+	}
+
+	return s.pgConfig
+}
+
+func (s *serviceProvider) GRPCConfig() config.GRPCConfig {
+	if s.grpcConfig == nil {
+		cfg, err := env.NewGRPCConfig()
+		if err != nil {
+			log.Fatalf("failed to get grpc config: %s", err.Error())
+		}
+
+		s.grpcConfig = cfg
+	}
+
+	return s.grpcConfig
+}
+
+func (s *serviceProvider) PgPool(ctx context.Context) *pgxpool.Pool {
+	if s.pgPool == nil {
+		pool, err := pgxpool.Connect(ctx, s.PGConfig().DSN())
+		if err != nil {
+			log.Fatalf("failed to connect to database: %v", err)
+		}
+
+		err = pool.Ping(ctx)
+		if err != nil {
+			log.Fatalf("ping error: %s", err.Error())
+		}
+		closer.Add(func() error {
+			pool.Close()
+			return nil
+		})
+
+		s.pgPool = pool
+	}
+
+	return s.pgPool
+}
+
+func (s *serviceProvider) UserRepository(ctx context.Context) repository.UserRepository {
+	if s.userRepository == nil {
+		s.userRepository = userRepository.NewRepository(s.PgPool(ctx))
+	}
+
+	return s.userRepository
+}
+
+func (s *serviceProvider) UserService(ctx context.Context) service.UserService {
+	if s.userService == nil {
+		s.userService = userService.NewService(s.UserRepository(ctx))
+	}
+
+	return s.userService
+}
+
+func (s *serviceProvider) UserImpl(ctx context.Context) *user.Implementation {
+	if s.userImpl == nil {
+		s.userImpl = user.NewImplementation(s.UserService(ctx))
+	}
+
+	return s.userImpl
+}
